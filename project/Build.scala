be7b3699d5d8507b76945209897bbec0213a7caf
yroffin <yroffin@gmail.com>
2013-09-21 08:53:09 +0200

    Passage en 2.1.4
    Signed-off-by: yroffin <yroffin@gmail.com>

--- project/Build.scala
+++ project/Build.scala
@@ -4,23 +4,25 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "jbehaviour-tools-ui"
-    val appVersion      = "1.0-SNAPSHOT"
+  val appName         = "jbehaviour-tools-ui"
+  val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-		javaCore,
-		javaJdbc,
-		javaEbean,
-		"org.springframework"    %    "spring-context"    %    "3.0.7.RELEASE",
-		"org.springframework"    %    "spring-core"       %    "3.0.7.RELEASE",
-		"org.springframework"    %    "spring-beans"      %    "3.0.7.RELEASE",
-		"org.tmatesoft.svnkit"   %    "svnkit-cli"        %    "1.7.6",
-		"com.github.yroffin"     %    "jbehaviour-engine" %    "0.0.2-SNAPSHOT"
-    )
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    javaCore,
+    javaJdbc,
+    javaEbean,
+	"org.springframework"    %    "spring-context"    %    "3.2.4.RELEASE",
+	"org.springframework"    %    "spring-core"       %    "3.2.4.RELEASE",
+	"org.springframework"    %    "spring-beans"      %    "3.2.4.RELEASE",
+	"org.tmatesoft.svnkit"   %    "svnkit-cli"        %    "1.7.6",
+	"com.github.yroffin"     %    "jbehaviour-tools" %     "0.0.2",
+	"com.github.yroffin"     %    "jbehaviour-engine" %    "0.0.2"
+  )
 
-   val main = play.Project(appName, appVersion, appDependencies).settings(
-		resolvers += "Local Maven Repository" at "file://u:/m2repo",
-		resolvers += "Default repository" at "http://repo1.maven.org/maven2"
-    )
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+	resolvers += "Default repository" at "http://192.168.0.130:9081/nexus/content/groups/public/"
+  )
 
 }
